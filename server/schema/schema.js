4d4ec2a9dee2ac5084b4d1e25a06bf9850d916af
Void GH <moltron_nano@hotmail.com>
2022-01-30 21:00:36 -0600

    feat: add dummy test db

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,24 @@
 const graphql = require('graphql')
+var _ = require('lodash')
+
+
+//dummy DB
+var userData = [
+    {id: '1', name: 'Bond' , age: 78 , kd: 15},
+    {id: '2', name: 'Bond2' , age: 78 , kd: 1},
+    {id: '3', name: 'Bond3' , age: 78 , kd: 15.2},
+    {id: '4', name: 'Bond4' , age: 78 , kd: 0.4},
+    {id: '55', name: 'Bond5' , age: 78 , kd: 1.33},
+    {id: '6', name: 'Bond6' , age: 78 , kd: 1.3},
+    {id: '7', name: 'Bond7' , age: 78 , kd: 14},
+]    
 
 const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
     GraphQLInt,
+    GraphQLFloat,
     GraphQLSchema
 } = graphql
 
@@ -15,7 +29,8 @@ const UserType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLString},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        kd: {type: GraphQLFloat}
     })
 })
 
@@ -29,6 +44,7 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
 
             resolve(parent, args){
+                return _.find(userData, {id: args.id})
 
                 //we resolve with data
                 //get and return data from datasource
